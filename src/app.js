7b15b6a9b75a5867adc2bd3580e341c518b45e6d
Pranav Bhatlapenumarthi <bhpranav18@gmail.com>
2026-03-28 16:53:56 +0530

    chore: dynamic backend port

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ const express = require("express");
 const generateRoute = require("./routes/generate");
 const cors = require("cors");
 
-
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/generate", generateRoute);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
