5b8053e7a5e2d7720a7337036b9a38973608583b
Pranav Bhatlapenumarthi <bhpranav18@gmail.com>
2026-03-29 17:02:36 +0530

    new: few path changes

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,6 +1,6 @@
 export async function generateBackend(prompt) {
 
-  const response = await fetch("http://localhost:3000/api/generate", {
+  const response = await fetch("http://100.48.212.89:3000/api/generate" || "http://3.237.71.17:3000/api/generate", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
